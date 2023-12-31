ffb22ecd277726d1b58d653b7628b1247c97ca9a
Jörg Schreiner <joerg.schreiner@gmail.com>
2023-12-31 14:47:08 +0100

    Improved greeting.

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello Earthling!\n');
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Hello Mars!\n');
+  res.send('Hello Marsian!\n');
 });
 
 app.listen(8080, function () {
