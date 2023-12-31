f1f865a45d52fe8d69a0f2b19964b6d0721a9047
Jörg Schreiner <joerg.schreiner@gmail.com>
2023-12-31 22:29:16 +0100

    Favicon works.

--- weather/app.js
+++ weather/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var favicon = require('serve-favicon')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')))
 
 app.use('/', indexRouter);
 
