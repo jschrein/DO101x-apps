bf803b0badde94a51056c9558c423477bf9a6c2e
Jörg Schreiner <joerg.schreiner@gmail.com>
2024-01-05 23:48:12 +0100

    Added output of hostname and address.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+    print "Hello, World! php version is " . PHP_VERSION . "\n";
+    print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
