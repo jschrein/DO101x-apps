7ce4bf2be164f5f6c5e04460463359d007798b64
Jörg Schreiner <joerg.schreiner@gmail.com>
2024-01-06 00:08:21 +0100

    Winter is coming.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
     print "Hello, World! php version is " . PHP_VERSION . "\n";
-    print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+    print "Winter is coming.\n";
 ?>
